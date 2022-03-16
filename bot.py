d8591560b79180b728a7099b6c22adcadbe98263
Thales Galvão <tera.thalespolo@gmail.com>
2022-03-16 01:31:43 -0300

    vanilla commit

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@
 #           nodemon: opicional, mas agiliza bastante no teste do código (compila auto sempre que salva o projeto)
 
 
+import asyncio
+from time import time
+from webbrowser import get
 import discord
 from discord.ext import commands
 
@@ -19,6 +22,10 @@ from discord.ext.commands.errors import MissingRequiredArgument, CommandNotFound
 #bot token
 TOKEN = os.environ["token"]
 
+SERVER_ID = os.environ["server_id"]
+
+ADMIN_ROLE = os.environ["admin_role"]
+
 #bot construction (bot prefix)
 intents = discord.Intents.all()
 
@@ -26,58 +33,69 @@ client = commands.Bot(".", intents=intents)
 
 channels = []
 
+Admin = None
+
 @client.event
 async def on_ready():
     print(f"{client.user} is online")
     #para saber onde está o canal o bot precisa estar logado antes de rodar "run"
 
-
 @client.event
 async def on_message(message):
         #para não entrar em loop das suas próprias mensagens
-        if message.content.startswith('!admin'):
-            if (message.author == message.server.owner):
-            # do stuff
-                await client.process_commands(message)
-            else:
-                msg = "This command can only be executed by the owner."
-        await client.send_message(message.channel, msg)
+        roles = message.author.roles
         if message.author == client.user:
+            return
+        if not ADMIN_ROLE in [y.name.lower() for y in roles]:
+            msg = await message.channel.send(
+                f"{message.author.name}, você não tem permissão necessária para fazer isso!"
+            )
+            await wait_delete(msg)
+            await wait_delete(message,3)
             return
         #quando detectar a palavra "palavrão"
         if "palavrão" in message.content:
-            await message.channel.send(
+            msg = await message.channel.send(
                 f"Por favor, {message.author.name}, não ofenda os demais usuários!"
             )
             await message.delete()
+            await wait_delete(msg)
         #evitar que o bot trave e os outros comandos parem de funcionar
+        await client.process_commands(message)
 
+async def wait_delete(msg, seconds=5):
+    await asyncio.sleep(seconds)
+    await msg.delete()
 
 @client.command(name="channel", help="Makes the channel visible/invisible to the bot. Arguments:\n<option>: on/off")
 async def activate_channel(ctx, id, option):
     global channels
+    msg = None
     if option in ["on", "off"]:
         if id in channels:
             if option == "on":
-                await ctx.send("Channel already online!")
+                msg = await ctx.send("Channel already online!")
             else:
                 index = channels.index(id)
                 channels.pop(index)
-                await ctx.send("Channel is now offline!")
+                msg = await ctx.send("Channel is now offline!")
         else:
             if option == "on":
                 channels.append(id)
-                await ctx.send("Channel is now online!")
+                msg = await ctx.send("Channel is now online!")
             else:
-                await ctx.send("Channel already offline!")
+                msg = await ctx.send("Channel already offline!")
     else:
-        await ctx.send("Wrong argument. Possible arguments: on/off")
+        msg = await ctx.send("Wrong argument. Possible arguments: on/off")
+    await wait_delete(msg)
+    await wait_delete(ctx.message,3)
 
 
 @client.command(name="close-channels", help="Close all channels activated")
-async def clear_channels():
+async def clear_channels(ctx):
     global channels
     channels.clear()
+    await wait_delete(ctx.message,3)
 
 
 ## função que cria o embed
@@ -93,21 +111,25 @@ async def create_embed(ctx):
     global channels
     for channel in channels:
         await client.get_channel(int(channel)).send(embed=build_embed())
+    await wait_delete(ctx.message,3)
 
 @client.command(name="clear", help="Clear x number of messages in the current channel")
-async def clear(ctx, amount=5):
+async def clear(ctx, amount=6):
     await ctx.channel.purge(limit=amount)
 
 
 @client.event
 async def on_command_error(ctx, error):
+    msg = None
     if isinstance(error, MissingRequiredArgument): #se o erro for falta de argumentos
-        await ctx.send("Favor enviar todos os Argumentos.")
+        msg = await ctx.send("Favor enviar todos os Argumentos.")
     elif isinstance(error, CommandNotFound): #se o erro for comando inexistente
-        await ctx.send("O comando não existe.")
+        msg = await ctx.send("O comando não existe.")
     else:
         return
-    ctx.send("Digite .help para ver os parâmetros de cada comando.")
+    msg = ctx.send("Digite .help para ver os parâmetros de cada comando.")
+    await wait_delete(msg)
+    await wait_delete(ctx.message,3)
 
 
 client.run(TOKEN)
